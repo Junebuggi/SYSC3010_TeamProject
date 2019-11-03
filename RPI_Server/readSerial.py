a912f552d2eb1751eb2ddb98ab9294b5d7ef0da1
Emma Boulay <emmaboulay@Emmas-MacBook-Pro.local>
2019-11-03 14:20:53 -0500

    Added Timer ISR for ack

--- RPI_Server/readSerial.py
+++ RPI_Server/readSerial.py
@@ -1,38 +1,32 @@
 import serial
-import RPi.GPIO as GPIO
 import time
-import sqlite3
-from datetime import datetime, date
 
-GPIO.setwarnings(False)
-#connect to database file
-dbconnect = sqlite3.connect("/home/pi/Documents/Team_Project/dataBases/plantNurseryDB.db");
-dbconnect.row_factory = sqlite3.Row;
-cursor = dbconnect.cursor();
-
-try:
-    ser = serial.Serial("/dev/ttyACM1", 9600)
-except: 
-    ser = serial.Serial("/dev/ttyACM0", 9600)
+ser = serial.Serial('/dev/tty.usbmodem144101', 9600)
     
 ser.baudrate=9600
-def blink(pin):
-    GPIO.output(pin,GPIO.HIGH)  
-    time.sleep(1)  
-    GPIO.output(pin,GPIO.LOW)  
-    time.sleep(1)  
-    return
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+ser.flushInput() 
+
+nPacket = 0
+
 while True:
-    tdate = str(date.today())
-    ttime = str(datetime.now().strftime("%H:%M:%S"))
+   # tdate = str(date.today())
+    #ttime = str(datetime.now().strftime("%H:%M:%S"))
     
     data=ser.readline().decode().strip('\r\n').split(",")
-    print(data)
+    if str(data[0]) == "8" and str(nPacket) == str(data[1]):
+        #message = "00" + "," + str(data[1]) + ","
+        #ser.write( bytes("00", 'utf-8'))
+        #ser.write(bytes(",", 'utf-8'))
+        #ser.write(bytes(str(data[1]), 'utf-8'))
+        #ser.write(bytes(",", 'utf-8'))
+        ack = "00," + str(nPacket) + "\0"
+        ser.write(ack.encode("utf-8"))
+        time.sleep(0.2)
+        #ser.write(str.encode(str())
+        print(data)
+        print(nPacket)
+        nPacket = (nPacket + 1) % 100
     
     #Database table: <RoomID>, <Light>, <Humidity>, <Temperature>, <Heat Index>, <Date>, <Time>
-    
-    cursor.execute("insert into sensorData values (" + str(data[0]) + ", '" + str(data[1]) + "', '" + str(data[2]) + "', '" + str(data[3]) + "', '" + str(data[4]) + "', '" + date + "', '" + ttime + "')");
-    
-    dbconnect.commit();
+    #cursor.execute("insert into sensorData values (" + str(data[0]) + ", '" + str(data[1]) + "', '" + str(data[2]) + "', '" + str(data[3]) + "', '" + str(data[4]) + "', '" + date + "', '" + ttime + "')");
+    #dbconnect.commit();
