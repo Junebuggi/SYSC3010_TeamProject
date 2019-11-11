29e506f9b7250d30c63be319d19c1a32ea55f27a
Ruqaya Almalki <ruqaya.almalki@carleton.ca>
2019-11-11 09:17:47 -0500

    added recyclerView for notifications View

--- Android_App/app/src/main/java/com/example/plantnursery/NotificationActivity.java
+++ Android_App/app/src/main/java/com/example/plantnursery/NotificationActivity.java
@@ -3,12 +3,33 @@ package com.example.plantnursery;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class NotificationActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<String> mDataset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mDataset = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            mDataset.add("Welcome to #" + i);
+        }
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setHasFixedSize(true);
+        mAdapter = new MainAdapter(mDataset);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
 
     }
 }
---
+++ Android_App/app/src/main/java/com/example/plantnursery/NotificationStorage.java
@@ -0,0 +1,10 @@
+package com.example.plantnursery;
+
+/**
+ * keeps track of all the notifications given
+ * shows notification on the notificationActivity
+ */
+
+public class NotificationStorage {
+
+}
